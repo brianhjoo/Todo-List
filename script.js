2c916aa8af17807b5de2d195f963ef3cd65a226e
Brian Joo <brianhjoo@gmail.com>
2023-02-14 19:49:14 -0800

    edit addTodo function to not use innerHTML and fix bug in searchForTodo

--- script.js
+++ script.js
@@ -2,29 +2,49 @@ const form = document.querySelector('.form');
 const list = document.querySelector('.list');
 const search = document.querySelector('#search');
 
+{/* <div class="todo">
+  <input class="todoInput" type="text" readonly="readonly" value="${input}"></input>
+  <div class="icons">
+    <i class="fa-regular fa-pen-to-square edit"></i>
+    <i class="fa-regular fa-trash-can delete"></i>
+  </div>
+</div>; */}
 
 function addTodo(input) {
-  const html = `
-    <div class="todo">
-      <input class="todoInput" type="text" readonly="readonly" value="${input}"></input>
-      <div class="icons">
-        <i class="fa-regular fa-pen-to-square edit"></i>
-        <i class="fa-regular fa-trash-can delete"></i>
-      </div>
-    </div>
-  `;
-  list.innerHTML += html;
+  const todoWrapper = document.createElement('div');
+  const todoInput = document.createElement('input');
+  const iconWrapper = document.createElement('div');
+  const editIcon = document.createElement('i');
+  const deleteIcon = document.createElement('i');
+
+  todoWrapper.classList.add('todo');
+
+  todoInput.classList.add('todoInput');
+  todoInput.type = 'text';
+  todoInput.autocomplete = 'off';
+  todoInput.setAttribute('readonly', 'readonly');
+  todoInput.setAttribute('value', input);
+
+  iconWrapper.classList.add('icons');
+  editIcon.classList.add('fa-regular', 'fa-pen-to-square', 'edit');
+  deleteIcon.classList.add('fa-regular', 'fa-trash-can', 'delete');
+
+  iconWrapper.append(editIcon, deleteIcon);
+  todoWrapper.append(todoInput, iconWrapper);
+  list.append(todoWrapper);
 }
 
 function searchForTodo(input) {
   const arr = [...list.children];
   arr.filter(todo => {
-    return !todo.textContent.toLowerCase().includes(input);
+    const todoInput = todo.querySelector('.todoInput');
+    return !todoInput.getAttribute('value').toLowerCase().includes(input);
   }).forEach(todo => {
     todo.classList.add('filtered');
   });
   arr.filter(todo => {
-    return todo.textContent.toLowerCase().includes(input);
+    const todoInput = todo.querySelector('.todoInput');
+    return todoInput.getAttribute('value').toLowerCase().includes(input);
   }).forEach(todo => {
     todo.classList.remove('filtered');
   });
